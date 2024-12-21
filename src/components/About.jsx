bad509e096f4b8b159ea71b2326e0aff21c3e33a
rajatbhaskare100 <80106274+RajatBhaskare7@users.noreply.github.com>
2024-12-22 00:14:09 +0530

    init app

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,35 +28,38 @@ const About = () => {
 
   return (
     <div id="about" className="min-h-screen w-screen">
-      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <p className="font-general text-sm uppercase md:text-[10px]">
-          Welcome to Zentry
+    <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+      <p className="font-general text-sm uppercase md:text-[20px] text-primary">
+        Welcome to Godlike Esports
+      </p>
+  
+      <AnimatedTitle
+        title="E<b>m</b>brace the <b>thrill</b> of<br /> comp<b>e</b>titive gaming"
+        containerClass="mt-5 !text-black text-center"
+      />
+  
+      <div className="about-subtext ">
+        {/* <p>
+          Enter the battlefield where legends are made and victories echo forever.
+        </p> */}
+        <p className="text-gray-500">
+          Godlike Esports is more than a gaming org—it's a community of champions, 
+          a legacy of dominance, and a gateway to the future of competitive gaming.
         </p>
-
-        <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
-          containerClass="mt-5 !text-black text-center"
-        />
-
-        <div className="about-subtext">
-          <p>The Game of Games begins—your life, now an epic MMORPG</p>
-          <p className="text-gray-500">
-            Zentry unites every player from countless games and platforms, both
-            digital and physical, into a unified Play Economy
-          </p>
-        </div>
-      </div>
-
-      <div className="h-dvh w-screen" id="clip">
-        <div className="mask-clip-path about-image">
-          <img
-            src="img/about.webp"
-            alt="Background"
-            className="absolute left-0 top-0 size-full object-cover"
-          />
-        </div>
       </div>
     </div>
+  
+    <div className="h-dvh w-screen" id="clip">
+      <div className="mask-clip-path about-image">
+        <img
+          src="img/heroxgod.jpeg"
+          alt="Background"
+          className="absolute left-0 top-0 size-full object-cover"
+        />
+      </div>
+    </div>
+  </div>
+  
   );
 };
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -141,31 +141,34 @@ const Hero = () => {
         </div>
 
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-          G<b>A</b>MING
+          Espor<b>ts</b>
         </h1>
+        
+
 
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
-              redefi<b>n</b>e
+              GodLi<b>k</b>e
             </h1>
 
-            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+            {/* <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
               Enter the Metagame Layer <br /> Unleash the Play Economy
-            </p>
+            </p> */}
 
-            <Button
+            {/* <Button
               id="watch-trailer"
               title="Watch trailer"
               leftIcon={<TiLocationArrow />}
               containerClass="bg-yellow-300 flex-center gap-1"
-            />
+            /> */}
           </div>
         </div>
       </div>
 
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        G<b>A</b>MING
+      Espor<b>ts</b>
+
       </h1>
     </div>
   );
